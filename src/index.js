f29ffce8e1125c7cfb8adb546fd364d937b08bae
zanchi <emzanchi@gmail.com>
2018-05-18 22:09:35 -0500

    Added comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 import { getAlbums, getPosts, getComments, getUsers } from './api';
 import { joinPostWithComments, joinUserWithAlbums, joinPostWithUser } from './utils';
 
+/* Takes the posts from typicode's JSON placeholder that come in the form of
+ * {
+ *   body,
+ *   id,
+ *   title,
+ *   userId,
+ * }
+ * and transforms it into
+ * {
+ *   body,
+ *   comments: [{
+ *     body,
+ *     email,
+ *     id (normalized to start at 0 for each post),
+ *     name,
+ *   }]
+ *   id,
+ *   title,
+ *   user: {
+ *     address: {...},
+ *     albums: [{
+ *       id (normalized to start at 0 for each user),
+ *       title
+ *     }],
+ *     company: {...},
+ *     id,
+ *     name,
+ *     phone,
+ *     username,
+ *     website,
+ *   }
+ * }
+ */
 (async () => {
   const [albums, comments, posts, users] =
     await Promise.all([getAlbums(), getComments(), getPosts(), getUsers()]);
