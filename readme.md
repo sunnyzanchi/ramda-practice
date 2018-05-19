120a4451248f4186ebc25f5a1f4f11e4274dcbad
zanchi <emzanchi@gmail.com>
2018-05-19 13:47:45 -0500

    Added native implementation alongside Ramda with performance logging

--- readme.md
+++ readme.md
@@ -8,3 +8,9 @@ To run, just run
 npm start
 ```
 Not really a lot to look at, it just logs some lightly transformed data :information_desk_person: :sweat_smile:
+
+## Performance
+I knew Ramda would be slower than native functions, I just wanted to see how much slower.
+I wrote an implementation using only native JS functionality and it's consistently 10+ times faster than Ramda :weary:
+tested in FireFox/Chrome. And, really, I think the complexity is about the same.
+So. Probably wouldn't use Ramda where performance counts :confused:
--- src/index.js
+++ src/index.js
@@ -34,13 +34,63 @@ import { joinPostWithComments, joinUserWithAlbums, joinPostWithUser } from './ut
  *   }
  * }
  */
+
+const removePostId = comment => {
+  const { postId, ...commentWithoutPostId } = comment;
+  return commentWithoutPostId;
+};
+
+const removeUserId = album => {
+  const { userId, ...albumWithoutUserId } = album;
+  return albumWithoutUserId;
+};
+
+const resetIds = (item, i) => ({ ...item, id: i });
+
+const ramdaTransform = (albums, comments, posts, users) => {
+  const postsWithComments = posts.map(joinPostWithComments(comments));
+  const usersWithAlbums = users.map(joinUserWithAlbums(albums));
+  const postsWithCommentsAndUsers = postsWithComments.map(joinPostWithUser(usersWithAlbums));
+  return postsWithCommentsAndUsers;
+};
+
+const nativeTransform = (albums, comments, posts, users) => {
+  const postsWithComments = posts.map(post => ({
+    ...post,
+    comments: comments
+      .filter(c => c.postId === post.id)
+      .map(resetIds)
+      .map(removePostId)
+  }));
+  const usersWithAlbums = users.map(user => ({
+    ...user,
+    albums: albums
+      .filter(a => a.userId === user.id)
+      .map(resetIds)
+      .map(removeUserId)
+  }));
+  const postsWithCommentsAndUsers = postsWithComments.map(post => {
+    const {userId, ...postWithoutUserId} = post;
+    return {
+      ...postWithoutUserId,
+      user: usersWithAlbums.find(u => u.id === post.id)
+    };
+  });
+  return postsWithCommentsAndUsers;
+};
+
 (async () => {
   const [albums, comments, posts, users] =
     await Promise.all([getAlbums(), getComments(), getPosts(), getUsers()]);
 
-  const postsWithComments = posts.map(joinPostWithComments(comments));
-  const usersWithAlbums = users.map(joinUserWithAlbums(albums));
-  const postsWithCommentsAndUsers = postsWithComments.map(joinPostWithUser(usersWithAlbums));
+  console.time('Ramda transform');
+  const ramdaResult = ramdaTransform(albums, comments, posts, users);
+  console.timeEnd('Ramda transform');
 
-  console.log(postsWithCommentsAndUsers);
+  console.time('Native transform');
+  const nativeResult = nativeTransform(albums, comments, posts, users);
+  console.timeEnd('Native transform');
+
+  console.log('nativeResult', nativeResult);
+  console.log('ramdaResult', ramdaResult);
 })();
